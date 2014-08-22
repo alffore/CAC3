f476395dee70a4729c8abfc20d307667701c0917
A. Alfonso F. R <alffore@gmail.com>
2014-08-22 08:27:39 -0500

    continua boceto

--- src/cuda/distancia.cu
+++ src/cuda/distancia.cu
@@ -3,16 +3,17 @@
 double RT = 6378390.00;
 
 
-__global__ void distancia(const float ploc_lon,const float ploc_lat,const float prec_lon,const float prec_lat, float distancia){
+__global__ void distancia(const float* ploc_lon,const float* ploc_lat,const float* prec_lon,const float* prec_lat, float* distancia){
 
 
+int id=threadIdx.x + blockDim.x * blockIdx.x;
 
     
-    distancia=sin(ploc_lat)*sin(prec_lat);
+    distancia=sin(pl_lat[i])*sin(pr_lat);
     
-    distancia+=cos(ploc_lat)*cos(ploc_lon)*cos(prec_lat)*cos(prec_lon);
+    distancia+=cos(pl_lat[i])*cos(ploc_lon)*cos(prec_lat)*cos(prec_lon);
     
-    distancia+=cos(ploc_lat)*sin(ploc_lon)*cos(prec_lat)*sin(prec_lon);
+    distancia+=cos(pl_lat[i])*sin(ploc_lon)*cos(prec_lat)*sin(prec_lon);
     
     distancia=acos(distancia);
 
